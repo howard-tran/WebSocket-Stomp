360a206de33422cc9f026c14739830eb725cd8b7
mingkhoi <mingkhoitran1234@gmail.com>
2020-11-15 17:00:12 +0700

    init web template

---
+++ Backend/JavaBackend/src/main/java/com/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+
+  @GetMapping("/")
+  public String homePage() {
+    return "home"; 
+  }
+}
